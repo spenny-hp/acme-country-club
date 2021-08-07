293c209b5847a2ea03f78e75caf031a256d78c45
Rowena Foo <rwnfoo@github.com>
2021-08-07 11:02:21 -0500

    2nd try

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_cc');
 
-const { STRING, INTEGER, UUID, UUIDV4 } = Sequelize.DataTypes;
+const { STRING, DATE, UUID, UUIDV4 } = Sequelize.DataTypes;
 
 const members = ['moe', 'lucy', 'larry', 'ethyl'];
 const facilities = ['tennis', 'ping-pong', 'raquet-ball', 'bowling'];
-
-async function syncAndSeed() {
-    await conn.sync({force:true});
-    await Promise.all(members.map(member => Member.create({name:member})));
-    await Promise.all(facilities.map(facility => Facility.create({name:facility})));
-}
 
 const Member = conn.define('member', {
     id: {
@@ -40,8 +34,33 @@ const Facility = conn.define('facility', {
             notEmpty: true
         }
     }
-});
+})
 
+const Booking = conn.define('facility', {
+    id: {
+        type: UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true
+    },
+    startTime:{},
+    endTime:{},
+    bookedById:{},
+    facilityId:{}
+    }
+})
+
+
+async function syncAndSeed() {
+    await conn.sync({force:true});
+    const [moe, lucy, larry, ethyl] = await Promise.all(members.map(name => Member.create({name})));
+    moe.sponsorId = lucy.id;
+    ethyl.sponsorId = moe.id;
+    larry.sponsorId = lucy.id;
+    await Promise.all([moe.save(),ethyl.save(),larry.save()])
+    await Promise.all(facilities.map(facility => Facility.create({name:facility})));
+}
+
+Member.hasMany(Member,{as: 'children', foreignKey: 'sponsorId'})
 
 module.exports = {
     syncAndSeed,
